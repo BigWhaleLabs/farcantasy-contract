b9eac98320c50be9654e3dc8e65054600b45ac5a
Nikita Kolmogorov <backmeupplz@gmail.com>
2023-03-28 13:15:26 -0700

    simplify deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,29 +1,36 @@
+import { Farcantasy, Farcantasy__factory } from 'typechain'
 import { ethers, run } from 'hardhat'
 import { utils } from 'ethers'
 import { version } from '../package.json'
 
-async function main() {
-  const [deployer] = await ethers.getSigners()
+const contractName = 'Farcantasy'
+const contractSymbol = 'FRCNTSY'
+const baseUri = 'https://metadata.farcantasy.xyz/metadata/'
+const waitTime = 60 * 1000
 
-  // Deploy the contract
+type ConstructorArguments = [string, string, string, string]
+
+async function printDeployer() {
+  const [deployer] = await ethers.getSigners()
   console.log('Deploying contracts with the account:', deployer.address)
   console.log(
     'Account balance:',
     utils.formatEther(await deployer.getBalance())
   )
+}
 
-  const contractName = 'Farcantasy'
-  const contractSymbol = 'FRCNTSY'
-  const baseUri = 'https://farcantasy.com/metadata/'
-  console.log(`Deploying ${contractName}...`)
-  const Contract = await ethers.getContractFactory(contractName)
-  const contract = await Contract.deploy(
-    contractName,
-    contractSymbol,
-    version,
-    baseUri
+function waitForBlockchainUpdate() {
+  console.log(
+    `Wait for ${waitTime / 1000} seconds to make sure blockchain is updated`
   )
+  return new Promise((resolve) => setTimeout(resolve, waitTime))
+}
 
+async function deployContract(
+  factory: Farcantasy__factory,
+  constructorArguments: ConstructorArguments
+) {
+  const contract = await factory.deploy(...constructorArguments)
   console.log(
     'Deploy tx gas price:',
     utils.formatEther(contract.deployTransaction.gasPrice || 0)
@@ -33,32 +40,48 @@ async function main() {
     utils.formatEther(contract.deployTransaction.gasLimit)
   )
   await contract.deployed()
+  return contract
+}
+
+async function verifyContract(
+  contract: Farcantasy,
+  constructorArguments: ConstructorArguments
+) {
   const address = contract.address
-
-  console.log('Contract deployed to:', address)
-  console.log('Wait for 1 minute to make sure blockchain is updated')
-  await new Promise((resolve) => setTimeout(resolve, 60 * 1000))
-
-  // Try to verify the contract on Etherscan
   console.log('Verifying contract on Etherscan')
   try {
     await run('verify:verify', {
       address,
-      constructorArguments: [contractName, contractSymbol, version, baseUri],
+      constructorArguments,
     })
+    console.log(`${contractName} deployed and verified on blockchain explorer!`)
   } catch (err) {
     console.log(
-      'Error verifiying contract on Etherscan:',
+      'Error verifying contract on Etherscan:',
       err instanceof Error ? err.message : err
     )
   }
-
-  // Print out the information
-  console.log(`${contractName} deployed and verified on blockchain explorer!`)
-  console.log('Contract address:', address)
 }
 
-main().catch((error) => {
-  console.error(error)
-  process.exitCode = 1
-})
+async function main() {
+  try {
+    await printDeployer()
+    const factory = await ethers.getContractFactory(contractName)
+    const constructorArguments = [
+      contractName,
+      contractSymbol,
+      version,
+      baseUri,
+    ] as ConstructorArguments
+    const contract = await deployContract(factory, constructorArguments)
+    console.log('Contract deployed to:', contract.address)
+    await waitForBlockchainUpdate()
+    await verifyContract(contract, constructorArguments)
+    console.log('Contract address:', contract.address)
+  } catch (error) {
+    console.error(error)
+    process.exitCode = 1
+  }
+}
+
+void main()
